f8123895cbe5a6579b379a2ac3ee6df3f5e7a7e3
Sam Dalton <spd4nf@virginia.edu>
2018-10-25 11:41:19 -0500

    create and updated schemas and seeded the databse

---
+++ models/folder.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const folderSchema = new mongoose.Schema({
+  name : {type: String, required: true, unique: true}
+});
+
+folderSchema.set('timestamps', true);
+
+const config = {
+  virtuals: true,
+  transform: (doc,result)=>{
+    delete result._id;
+    delete result._v;
+  }
+}
+
+folderSchema.set('toObject', config);
+folderSchema.set('toJSON', config);
+
+module.exports = mongoose.model('Folder', folderSchema);
--- models/notes.js
+++ models/notes.js
@@ -4,18 +4,23 @@ const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  content: String
+  content: String,
+  folderId : {type: mongoose.Schema.Types.ObjectId, ref: 'Folder' }
 });
 
 // Add `createdAt` and `updatedAt` fields
 noteSchema.set('timestamps', true);
 
-noteSchema.set('toObject', {
+const config = {
   virtuals: true,     // include built-in virtual `id`
-  transform: (doc, ret) => {
-    delete ret._id; // delete `_id`
-    delete ret.__v;
+  transform: (doc, result) => {
+    // result.id = doc._id;
+    delete result._id;
+    delete result.__v;
   }
-});
+};
+
+noteSchema.set('toObject', config);
+noteSchema.set('toJSON', config);
 
 module.exports = mongoose.model('Note', noteSchema);
